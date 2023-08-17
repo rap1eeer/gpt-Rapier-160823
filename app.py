e847e29150b3b24264c7fe1430a436ef5157faba
rap1eeer <141307152+rap1eeer@users.noreply.github.com>
2023-08-17 09:20:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import openai
 
 app = Flask(__name__)
 
-openai.api_key = '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'
+openai.api_key = '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'
 
 
 @app.route("/", methods=["GET", "POST"])
